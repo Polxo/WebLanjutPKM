623b4af6d703b9cd80260cb3a751f959eef60b4b
luqman00 <muhammad.shubkhi@students.amikom.ac.id>
2018-12-17 22:47:10 +0700

    Muhammad Luqman Ash Shubkhi
    update

---
+++ index.php
@@ -0,0 +1,152 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Culture of Indonesia</title>
+	<!--Bostrap-->
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+  	<!--end bostrap-->
+   	<!-- Start WOWSlider.com HEAD section -->
+		<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+		<script type="text/javascript" src="engine1/jquery.js"></script>
+	<!-- End WOWSlider.com HEAD section -->
+	<!--css-->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<!--end css-->
+	<!--angular-->
+  	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+  	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular-route.js"></script>
+  	<!-- angular end--> 
+
+</head>
+<body class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<header>
+				<!-- menu -->
+				<div class="menu">
+					<nav class="navbar navbar-expand-lg navbar-light bg-light ">
+					  <a class="navbar-brand" href="#">Culture Of indonesia</a>
+					  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" 	aria-expanded="false" aria-label="Toggle navigation">
+					  	<span class="navbar-toggler-icon"></span>
+					  </button>
+					  	<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+						    <div class="navbar-nav">
+						   	   	<a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+						      	<a class="nav-item nav-link" href="#">Kebudayaan</a>
+						      	<a class="nav-item nav-link" href="#">Pariwisata</a>
+						      	<a class="nav-item nav-link" href="#">Login</a>	
+					    	</div>
+					  	</div>
+					</nav>
+				</div>
+				<!--end menu-->
+			</header>
+			<content>
+							<!--slogan-->
+				<div class="slogan">
+					<div class="pslogan">
+						<span class="border">
+							dfsadsadsadushaduhsahdoisahdiosahdihdoi
+						</span>
+					</div>
+				</div>
+				<!--end slogan-->
+				<!--video-->
+					<div class="video">
+						<iframe width="100%" height="500"  src="https://www.youtube.com/embed/O4yc069cPiw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+						
+					</div>
+				<!--end video-->
+				<!--deskripsi-->
+				<div class="deskripsi">
+					<h1>sadasdasdasdasdasd</h1>
+					<p>sadsadsadsadasdsa</p>
+
+				</div>
+				<!--deskripsi end-->
+			<!--slider-->
+				<div class="slider">
+					<div id="wowslider-container1">
+								<div class="ws_images">
+									<ul>
+										<li><a href="http://wowslider.net" target="_self"><img src="data1/images/1.jpg" alt="javascript slider" title="1" id="wows1_0"/></a></li>
+										<li><img src="data1/images/2.jpg" alt="2" title="2" id="wows1_1"/></li>
+									</ul>
+								</div>
+								<div class="ws_bullets">
+									<div>
+										<a href="#" title="1"><span><img src="data1/tooltips/1.jpg" alt="1"/>1</span></a>
+										<a href="#" title="2"><span><img src="data1/tooltips/2.jpg" alt="2"/>2</span></a>
+									</div>
+								</div>
+								<div class="ws_script" style="position:absolute;left:-99%">
+									<a href="http://wowslider.net">bootstrap carousel</a>by WOWSlider.com v8.8
+								</div>
+								<div class="ws_shadow">
+								</div>
+							</div>	
+							<script type="text/javascript" src="engine1/wowslider.js"></script>
+							<script type="text/javascript" src="engine1/script.js"></script>
+							<!-- End WOWSlider.com BODY section -->
+				</div>
+				<!--slider end-->
+			</content>
+			<footer class="page-footer font-small blue pt-4 ">
+
+  <!-- Footer Links -->
+  <div class="container-fluid text-center text-md-left">
+
+    <!-- Grid row -->
+    <!--div class="row"-->
+      <!-- Grid column -->
+      <div class="col-md-6 mt-md-0 mt-3">
+
+        <!-- Content -->
+        <h5 class="text-uppercase">Kontak </h5>
+        <p>Alamat: Jln wijaya Kusuma No 21</p>
+        <p>Provinsi: Yogyakarta</p>
+        <p>Telepon: (0376) 23626</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none pb-3">
+
+      <!-- Grid column -->
+      <div class="col-md-6 mb-md-0 mb-3">
+
+      <div style="text-align: right;"> <!-- Links -->
+        <h5 class="text-uppercase">Links 	:</h5>
+          <img style="width: 50px;height: 50px;" src="img/facebook.png">
+          <img style="width: 50px;height: 50px;" src="img/instagram.png">
+          <img style="width: 50px;height: 50px;" src="img/email.png">
+          <img style="width: 50px;height: 50px;" src="img/twitter.png">
+ 
+       </div> <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  <!--/div-->
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2018 Copyright:
+    <a href="https://mdbootstrap.com/bootstrap-tutorial/">Cluture Of Indonesia</a>
+  </div>
+  <!-- Copyright -->
+
+</footer>
+		</div>
+		
+	</div>
+
+
+
+</body>
+</html>
