d99b0e7fa31286fa54e7fd7d524ffbe90aacdc56
LambangAR <lambang.ro@students.amikom.ac.id>
2018-12-25 18:44:02 +0700

    fix kon.php

--- adm/kon.php
+++ adm/kon.php
@@ -1,7 +1,10 @@
 <?php
 
-mysqli_connect('localhost','root');
-mysql_select_db('coi');
+$server ='localhost';
+$username ='root';
+$password ='';
+$database ='coi';
 
-$con = mysqli_connect('localhost', 'root', '', 'coi');
+$con = mysqli_connect($server,$username,$password) or die("koneksi gagal");
+mysqli_select_db($con, $database)or die("database belum ada,silahkan import database");
 ?>
